fb546bd96d2e2e7b9661f053ec24c3c830cb975e
David Anderson <david.anderson.sil@gmail.com>
2019-09-13 00:52:02 -0300

    chore: Add script to build and run application container

---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --force-rm --no-cache --rm -t house-prices-prediction .
+
+# Step 2: 
+# List docker images
+
+# Step 3: 
+# Run flask app
+docker run -p 8000:80 house-prices-prediction
